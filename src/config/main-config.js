68f27b9f62c058be3ef02f2964186c2bdc59a2c3
katkinso <katyratkinson@yahoo.com>
2019-12-01 09:20:46 -0800

    update

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -23,7 +23,7 @@ module.exports = {
         "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
         "preflightContinue": false,
         "optionsSuccessStatus": 204,
-        credentials: true
+        "credentials": true
     
     }));
     // app.use(cors({
